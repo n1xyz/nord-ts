a4275a2d89e131e97d14b28a1880fb34649e9fed
Sheheryar Parvaz <me@cherryman.org>
2025-10-29 17:33:31 -0400

    proton-ts: bump v0.0.5

--- src/nord/client/Nord.ts
+++ src/nord/client/Nord.ts
@@ -238,7 +238,7 @@ export class Nord {
     const connection = new Connection(solanaUrl, { commitment: "confirmed" });
     const protonClient = await ProtonClient.init({
       protonUrl: webServerUrl,
-      app: new PublicKey(app),
+      bridgeVk: new PublicKey(app),
       solConn: connection,
     });
     const nord = new Nord({
